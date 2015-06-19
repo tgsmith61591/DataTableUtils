3be1acf7ea20100e49bdd5383a1f2667185a0ab3
ts749c <tgsmith61591@gmail.com>
2015-06-19 10:14:50 -0500

    Added more user params

--- DataTableWritable.cs
+++ DataTableWritable.cs
@@ -11,11 +11,21 @@ namespace DataTableUtilities {
 			LEFT
 		}
 
-		public readonly Int32 DEFAULT_HEAD = 6;
-		public readonly PrintJustifier DEFAULT_PRINT_JUSTIFICATION = PrintJustifier.RIGHT;
+
+		public static readonly Int32 DEFAULT_WHITE_SPACE = 4;
+		public static readonly Int32 DEFAULT_HEAD = 6;
+		public static readonly PrintJustifier DEFAULT_PRINT_JUSTIFICATION = PrintJustifier.RIGHT;
+		public static readonly Char DEFAULT_WHITE_SPACE_CHAR = ' ';
+
 		public PrintJustifier PrintJustification { get; set; }
+		private Int32 WhiteSpace = DEFAULT_WHITE_SPACE; //Set as private because must handle < 1 exception
+		public Char WhiteSpaceChar { get; set; }
+		public bool PrintHeaders { get; set; }
+
 		public DataTableWritable () : base() {
 			this.PrintJustification = DEFAULT_PRINT_JUSTIFICATION;
+			this.WhiteSpaceChar = DEFAULT_WHITE_SPACE_CHAR;
+			this.PrintHeaders = true;
 		}
 
 		public String[] Headers() {
@@ -40,6 +50,12 @@ namespace DataTableUtilities {
 			new Printer (this).Print (Math.Min(RowCount, RowMax));
 		}
 
+		public void SetWhiteSpace(Int32 Space) {
+			if (Space < 1)
+				throw new ArgumentException (Space+" cannot be less than 1");
+			this.WhiteSpace = Space;
+		}
+
 		public void TogglePrinterJustification() {
 			PrintJustification = PrintJustification == DEFAULT_PRINT_JUSTIFICATION ? PrintJustifier.LEFT : PrintJustifier.RIGHT;
 		}
@@ -47,7 +63,6 @@ namespace DataTableUtilities {
 
 
 		private sealed class Printer {
-			private readonly static Int32 WHITE_SPACE = 4;
 			private bool RightJustify;
 			private String[] Headers;
 			DataTableWritable Table;
@@ -75,7 +90,7 @@ namespace DataTableUtilities {
 				int RowNum = 0;
 				while(RowNum < RowMax) {
 					StringBuilder sb = new StringBuilder ();
-					if(RowNum == 0) {
+					if(RowNum == 0 && Table.PrintHeaders) {
 						foreach (String header in Headers)
 							sb.Append (WhiteSpaceFormattedString (header, HeaderToWidth [header]));
 
@@ -92,15 +107,15 @@ namespace DataTableUtilities {
 				}
 			}
 
-			private static String WhiteSpace(Int32 Length) {
+			private static String WhiteSpace(Int32 Length, Char WhiteSpaceChar) {
 				Char[] c = new Char[Length];
 				for(int i = 0; i < Length; i++)
-					c[i] = ' ';
+					c[i] = WhiteSpaceChar;
 				return new String ( c );
 			}
 
 			private String WhiteSpaceFormattedString(String Raw, Int32 ColumnWidth) {
-				String w = WhiteSpace (ColumnWidth + WHITE_SPACE - Raw.Length);
+				String w = WhiteSpace (ColumnWidth + Table.WhiteSpace - Raw.Length, this.Table.WhiteSpaceChar);
 				return RightJustify ? w + Raw : Raw + w;
 			}
 		}
