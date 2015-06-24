bec7f15e4dfa71a9ae29099f3d1b81a8d5e9f819
ts749c <tgsmith61591@gmail.com>
2015-06-24 16:00:22 -0500

    Added more settable parameters, implemented Serializable

---
+++ DataTableSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DataTableUtilities {
+	public class DataTableSerializer {
+		public static byte[] GetBaseStructBytes(DataTable dt) {
+			byte[] binaryDataResult = null;
+			using(MemoryStream ms = new MemoryStream()) {
+				BinaryFormatter bf = new BinaryFormatter ();
+				dt.RemotingFormat = SerializationFormat.Binary;
+				bf.Serialize(ms, dt);
+				binaryDataResult = ms.ToArray ();
+			}
+
+			return binaryDataResult;
+		}
+
+		public static DataTable GetBaseStructFromBytes(byte[] bytes) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			MemoryStream ms = new MemoryStream (bytes);
+			DataTable dt = (DataTable)bf.Deserialize (ms);
+			ms.Close ();
+			return dt;
+		}
+	}
+}
--- DataTableWritable.cs
+++ DataTableWritable.cs
@@ -3,34 +3,100 @@ using System.Data;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace DataTableUtilities {
+	[Serializable()]
 	public class DataTableWritable : DataTable {
 		public enum PrintJustifier {
 			RIGHT,
 			LEFT
 		}
 
+		/// <summary>
+		/// Used for deserialization purposes
+		/// </summary>
+		private static readonly Dictionary<int, PrintJustifier> PrintJustifierBitMaskDeserializer = new Dictionary<int, PrintJustifier> () {
+			{0, PrintJustifier.RIGHT},
+			{1, PrintJustifier.LEFT}
+		};
 
-		public static readonly Int32 DEFAULT_WHITE_SPACE = 4;
-		public static readonly Int32 DEFAULT_HEAD = 6;
+		/// <summary>
+		/// Used for serialization purposes
+		/// </summary>
+		private static readonly Dictionary<PrintJustifier, int> PrintJustifierBitMaskSerializer = new Dictionary<PrintJustifier, int> () {
+			{PrintJustifier.RIGHT, 0},
+			{PrintJustifier.LEFT,  1}
+		};
+
+
+		public static readonly int DEFAULT_WHITE_SPACE = 4;
+		public static readonly int DEFAULT_HEAD = 6;
 		public static readonly PrintJustifier DEFAULT_PRINT_JUSTIFICATION = PrintJustifier.RIGHT;
-		public static readonly Char DEFAULT_WHITE_SPACE_CHAR = ' ';
+		public static readonly char DEFAULT_WHITE_SPACE_CHAR = ' ';
 
+		/// <summary>
+		/// Holds the printer justification
+		/// </summary>
+		/// <value>The print justification.</value>
 		public PrintJustifier PrintJustification { get; set; }
-		private Int32 WhiteSpace = DEFAULT_WHITE_SPACE; //Set as private because must handle < 1 exception
-		public Char WhiteSpaceChar { get; set; }
+
+		/* These variables will be serialized */
+		private int PrintJustificationBitMask { get; set; }
+		private int WhiteSpace = DEFAULT_WHITE_SPACE; //Set as private because must handle < 1 exception
+		public char WhiteSpaceChar { get; set; }
 		public bool PrintHeaders { get; set; }
 
-		public DataTableWritable () : base() {
+
+		protected DataTableWritable(SerializationInfo info, StreamingContext context) : base(info, context) { init (); }
+		public DataTableWritable () : base() { init (); }
+		public DataTableWritable(DataTable data) : this() {
+			DataTable d = data.Copy ();
+
+			String[] headers = Headers (d);
+			foreach(String header in headers)
+				this.Columns.Add (new DataColumn (header, d.Columns[header].DataType));
+
+			foreach (DataRow row in d.Rows) {
+				DataRow dr = this.NewRow ();
+
+				foreach (String header in headers)
+					dr [header] = row [header];
+
+				this.Rows.Add (dr);
+			}
+		}
+
+
+		private void init() {
 			this.PrintJustification = DEFAULT_PRINT_JUSTIFICATION;
+			this.PrintJustificationBitMask = PrintJustifierBitMaskSerializer [this.PrintJustification];
+
 			this.WhiteSpaceChar = DEFAULT_WHITE_SPACE_CHAR;
 			this.PrintHeaders = true;
 		}
 
+
+
+		public new DataTableWritable Copy() {
+			DataTableWritable db = (DataTableWritable) base.Copy ();
+			db.PrintJustification = this.PrintJustification;
+			db.WhiteSpace = this.WhiteSpace;
+			db.WhiteSpaceChar = this.WhiteSpaceChar;
+			db.PrintHeaders = this.PrintHeaders;
+
+			return db;
+		}
+
+		public static String[] Headers(DataTable data) {
+			return (from dc in data.Columns.Cast<DataColumn>()
+				select dc.ColumnName).ToArray();
+		}
+
 		public String[] Headers() {
-			return (from dc in this.Columns.Cast<DataColumn>()
-				select dc.ColumnName).ToArray();;
+			return Headers (this);
 		}
 
 		public void Print() {
@@ -50,6 +116,35 @@ namespace DataTableUtilities {
 			new Printer (this).Print (Math.Min(RowCount, RowMax));
 		}
 
+		public static DataTableWritable ReadFromByteStream(byte[] bytes) {
+			using (MemoryStream m = new MemoryStream(bytes)) {
+				using (BinaryReader reader = new BinaryReader(m)) {
+					int length = reader.ReadInt32 ();
+					byte[] datatable = reader.ReadBytes (length);
+					DataTable baseStruct = DataTableSerializer.GetBaseStructFromBytes (datatable);
+					DataTableWritable result = new DataTableWritable(baseStruct);
+
+					result.PrintJustificationBitMask = reader.ReadInt32();
+					result.PrintJustification = PrintJustifierBitMaskDeserializer[result.PrintJustificationBitMask];
+
+					result.WhiteSpace = reader.ReadInt32 ();
+					result.WhiteSpaceChar = reader.ReadChar ();
+					result.PrintHeaders = reader.ReadBoolean ();
+
+					return result;
+				}
+			}
+		}
+
+		public static Type[] Schema(DataTable data) {
+			return (from dc in data.Columns.Cast<DataColumn>()
+				select dc.DataType).ToArray();
+		}
+
+		public Type[] Schema() {
+			return Schema (this);
+		}
+
 		public void SetWhiteSpace(Int32 Space) {
 			if (Space < 1)
 				throw new ArgumentException (Space+" cannot be less than 1");
@@ -58,10 +153,31 @@ namespace DataTableUtilities {
 
 		public void TogglePrinterJustification() {
 			PrintJustification = PrintJustification == DEFAULT_PRINT_JUSTIFICATION ? PrintJustifier.LEFT : PrintJustifier.RIGHT;
+			this.PrintJustificationBitMask = PrintJustifierBitMaskSerializer [this.PrintJustification];
+		}
+
+		public byte[] WriteToByteStream() {
+			using (MemoryStream m = new MemoryStream()) {
+				using (BinaryWriter writer = new BinaryWriter(m)) {
+					byte[] dtbytes = DataTableSerializer.GetBaseStructBytes ((DataTable)this);
+					writer.Write((Int32)dtbytes.Length);
+					writer.Write(dtbytes);
+
+					writer.Write(PrintJustificationBitMask);
+					writer.Write(WhiteSpace);
+					writer.Write(WhiteSpaceChar);
+					writer.Write(PrintHeaders);
+				}
+				return m.ToArray();
+			}
 		}
 
 
 
+
+		/// <summary>
+		/// Private inner class to handle all prints
+		/// </summary>
 		private sealed class Printer {
 			private bool RightJustify;
 			private String[] Headers;
